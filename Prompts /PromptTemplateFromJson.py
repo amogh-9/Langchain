654894ec06644fd827457f12650168fcba53af1e
amogh-9 <97966042+amogh-9@users.noreply.github.com>
2025-11-13 14:47:10 +0530

    Create PromptTemplateFromJson.py

---
+++ Prompts /PromptTemplateFromJson.py
@@ -0,0 +1,11 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import load_prompt
+from dotenv import load_dotenv
+
+model = ChatGoogleGenerativeAI(model='gemini-2.0-flash')
+prompt = load_prompt('template.json')
+
+final_prompt = prompt.format(profession="gamer",topic="lurking")
+
+responce = model.invoke(final_prompt)
+print(response.content)
