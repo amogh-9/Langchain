b2ef0ae8431684d01177b24efcb5c28d1bbe0c75
amogh-9 <97966042+amogh-9@users.noreply.github.com>
2025-11-19 12:47:26 +0530

    Delete Models directory

--- Models/EmbeddingModely.py
+++
@@ -1,8 +0,0 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from dotenv import load_dotenv
-
-load_dotenv()
-model = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
-
-vector = model.embed_query("hello world!")
-print(vector)
